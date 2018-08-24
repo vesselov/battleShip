48c4f54af070f3bcac508730a3586607a415ef47
vesselov <vesselov.zhenya@gmail.com>
2018-08-24 15:24:13 +0500

    js

--- main.js
+++ main.js
@@ -13,6 +13,21 @@ let view =  {
         cell.setAttribute('class', 'miss');
     }
 }
-view.displayHit('00');
-view.displayMiss('26');
-view.displayMessage('программа работает');
+
+let model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+    ships = [{location:['10', '20', '30'], hits : ['hit','',''] },
+            {location:['32','33','34'], hits:['', '','' ]},
+            {location:['42','43','44'], hits:['', '',''] }],
+    fire: function(guess){
+        for(var i;i < this.numShips;i++){
+            var ship = this.ships[i];
+        }
+    }        
+}
+// view.displayHit('00');
+// view.displayMiss('26');
+// view.displayMessage('программа работает');
